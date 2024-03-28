d3cd9c125fef728c191dc15770c418be1c6a29da
Navjot singh <navjot.s@idsil.com>
2024-03-28 17:52:22 +0530

    changes

--- Frontend/src/components/RequestMoney/index.jsx
+++ Frontend/src/components/RequestMoney/index.jsx
@@ -1,6 +1,7 @@
 import { useState, } from 'react'
 import Header from "../Dashboard/DashboardHeader/index";
 import { Link } from 'react-router-dom';
+import Footer from "../Dashboard/DashboardFooter/index"
 
 
 function index() {
@@ -10,7 +11,6 @@ function index() {
         <>
             <div id="main-wrapper">
                 <Header />
-
 
                 <div id="content" className="py-4">
                     <div className="container">
@@ -101,43 +101,7 @@ function index() {
                     </div>
                 </div>
 
-
-
-                <footer id="footer">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg d-lg-flex align-items-center">
-                                <ul className="nav justify-content-center justify-content-lg-start text-3">
-                                    <li className="nav-item"> <a className="nav-link active" href="#">About Us</a></li>
-                                    <li className="nav-item"> <a className="nav-link" href="#">Support</a></li>
-                                    <li className="nav-item"> <a className="nav-link" href="#">Help</a></li>
-                                </ul>
-                            </div>
-                            <div className="col-lg d-lg-flex justify-content-lg-end mt-3 mt-lg-0">
-                                <ul className="social-icons justify-content-center">
-                                    <li className="social-icons-facebook"><a data-bs-toggle="tooltip" href="http://www.facebook.com/" target="_blank" title="Facebook"><i className="fab fa-facebook-f"></i></a></li>
-                                    <li className="social-icons-twitter"><a data-bs-toggle="tooltip" href="http://www.twitter.com/" target="_blank" title="Twitter"><i className="fab fa-twitter"></i></a></li>
-                                    <li className="social-icons-google"><a data-bs-toggle="tooltip" href="http://www.google.com/" target="_blank" title="Google"><i className="fab fa-google"></i></a></li>
-                                    <li className="social-icons-youtube"><a data-bs-toggle="tooltip" href="http://www.youtube.com/" target="_blank" title="Youtube"><i className="fab fa-youtube"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="footer-copyright pt-3 pt-lg-2 mt-2">
-                            <div className="row">
-                                <div className="col-lg">
-                                    <p className="text-center text-lg-start mb-2 mb-lg-0">Copyright &copy; 2022 <a href="#">xyz</a>. All Rights Reserved.</p>
-                                </div>
-                                <div className="col-lg d-lg-flex align-items-center justify-content-lg-end">
-                                    <ul className="nav justify-content-center">
-                                        <li className="nav-item"> <a className="nav-link active" href="#">Security</a></li>
-                                        <li className="nav-item"> <a className="nav-link" href="#">Terms</a></li>
-                                        <li className="nav-item"> <a className="nav-link" href="#">Privacy</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+                <Footer />
             </div>
         </>
     )
